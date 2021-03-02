9293a619544d0def194248cd12ccbb0ca154c0c8
MarcosGOFavaretto <marcos.favaretto@etec.sp.gov.br>
2021-03-01 23:12:55 -0300

    UPD - Limpeza de diretivas não utilizadas

--- ProgramaGerenciadorDeVendas/src/View/TelaPrincipal.java
+++ ProgramaGerenciadorDeVendas/src/View/TelaPrincipal.java
@@ -21,7 +21,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Icon;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
